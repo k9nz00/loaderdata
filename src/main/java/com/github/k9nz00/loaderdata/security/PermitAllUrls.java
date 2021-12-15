04bea60772564fa742caf0d7b02377851ae36da9
Andrey Semka <k9nz00@yandex.ru>
2021-12-15 23:25:25 +0300

    feature/LOAD-14 remove redundant method into PermitAllUrls

--- src/main/java/com/github/k9nz00/loaderdata/security/PermitAllUrls.java
+++ src/main/java/com/github/k9nz00/loaderdata/security/PermitAllUrls.java
@@ -6,13 +6,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PermitAllUrls {
 
-    public final static String API_URL_LOGIN = "/api/users/login";
-
-    private static String[] permitUrls = new String[]{
-            API_URL_LOGIN,
-    };
+    public static final String API_URL_LOGIN = "/api/users/login";
 
     public static String[] getPermitUrls() {
-        return permitUrls;
+        return new String[]{
+                API_URL_LOGIN,
+        };
     }
 }
