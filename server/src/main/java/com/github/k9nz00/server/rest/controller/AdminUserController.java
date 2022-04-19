0c8ac6ca16de05c316f2d6ac2d12a3b9ab6c7271
Andrey Semka <k9nz00@yandex.ru>
2022-04-19 21:56:51 +0300

    feature/LOAD-60  added swagger description

--- server/src/main/java/com/github/k9nz00/server/rest/controller/AdminUserController.java
+++ server/src/main/java/com/github/k9nz00/server/rest/controller/AdminUserController.java
@@ -3,8 +3,16 @@ package com.github.k9nz00.server.rest.controller;
 import com.github.k9nz00.server.rest.dto.*;
 import com.github.k9nz00.server.service.AdminUserService;
 import com.github.k9nz00.server.util.UITransformers;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.api.annotations.ParameterObject;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,32 +24,65 @@ import static com.github.k9nz00.server.security.Authorities.MANAGE_USERS;
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
+@Tag(
+        name = "AdminUserController",
+        description = "Контроллер для административных работ. " +
+                "Предоставляет максимальный функционал для управления пользователями. " +
+                "При расширении функционала необходимо обновить описание")
 public class AdminUserController {
 
     private final AdminUserService userService;
 
     @Secured(MANAGE_USERS)
     @GetMapping("/users")
-    public Collection<UserDto> getAllUsers(@ParameterObject final UserCriteriaDto userCriteriaDto) {
+    @Operation(
+            summary = "Позвляет получить всех пользователей"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Возвращает список зарегистрированных пользователей")
+    })
+    public Collection<UserDto> getAllUsers(@ParameterObject @Parameter final UserCriteriaDto userCriteriaDto) {
         return userService.getAllUsers(userCriteriaDto, UITransformers::userDto);
     }
 
     @Secured(MANAGE_USERS)
     @PostMapping("/users")
+    @Operation(
+            summary = "Создание пользователя"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Пользователь успешно создан")
+    })
     public CurrentUserDto createUser(@Valid @RequestBody UserCreateDto userDto) {
         return userService.createUser(userDto);
     }
 
     @Secured(MANAGE_USERS)
     @GetMapping("/users/{id}")
-    public UserDto getUser(@PathVariable int id) {
+    @Operation(summary = "Получение информации по конкретному пользователю")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Полная информация о пользователе")
+    })
+    public UserDto getUser(
+            @PathVariable
+            @Parameter(
+                    name = "id",
+                    example = "1",
+                    description = "Идентификатор пользователя",
+                    required = true) int id) {
         return userService.getUser(id);
     }
 
     @Secured(MANAGE_USERS)
     @PutMapping("/users/{id}")
     public UserDto updateUser(@PathVariable int id,
-                              @Valid @ParameterObject @RequestBody UserUpdateDto updateDto) {
+                              @Valid @RequestBody UserUpdateDto updateDto) {
         return userService.updateUser(id, updateDto, UITransformers::userDto);
     }
 
--- server/src/main/java/com/github/k9nz00/server/rest/dto/UserUpdateDto.java
+++ server/src/main/java/com/github/k9nz00/server/rest/dto/UserUpdateDto.java
@@ -1,6 +1,5 @@
 package com.github.k9nz00.server.rest.dto;
 
-import com.github.k9nz00.server.validation.CheckUserExistsValidator;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
